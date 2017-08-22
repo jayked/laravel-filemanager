6ab4927ed098433f074b45ee3578b670024b4d9f
Jan-Joost <janjoost@u-lab.nl>
2017-08-22 15:24:14 +0200

    [PROVIDER] Fix for using singleton since Laravel 5.3

--- src/LaravelFilemanagerServiceProvider.php
+++ src/LaravelFilemanagerServiceProvider.php
@@ -3,55 +3,58 @@
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Config;
 
-
 /**
  * Class LaravelFilemanagerServiceProvider
+ *
  * @package Unisharp\Laravelfilemanager
  */
-class LaravelFilemanagerServiceProvider extends ServiceProvider {
+class LaravelFilemanagerServiceProvider extends ServiceProvider
+{
 
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        if (Config::get('lfm.use_package_routes'))
-            include __DIR__ . '/routes.php';
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		if( Config::get( 'lfm.use_package_routes' ) )
+		{
+			include __DIR__ . '/routes.php';
+		}
 
-        $this->loadTranslationsFrom(__DIR__.'/lang', 'laravel-filemanager');
+		$this->loadTranslationsFrom( __DIR__ . '/lang', 'laravel-filemanager' );
 
-        $this->loadViewsFrom(__DIR__.'/views', 'laravel-filemanager');
+		$this->loadViewsFrom( __DIR__ . '/views', 'laravel-filemanager' );
 
-        $this->publishes([
-            __DIR__ . '/config/lfm.php' => base_path('config/lfm.php'),
-        ], 'lfm_config');
+		$this->publishes( [
+			__DIR__ . '/config/lfm.php' => base_path( 'config/lfm.php' ),
+		], 'lfm_config' );
 
 		$this->mergeConfigFrom(
-			__DIR__.'/config/lfm.php' , base_path('u-cms-content')
+			__DIR__ . '/config/lfm.php', base_path( 'u-cms-content' )
 		);
 
-        $this->publishes([      
-            __DIR__.'/../public' => public_path('vendor/laravel-filemanager'),     
-        ], 'lfm_public');
+		$this->publishes( [
+			__DIR__ . '/../public' => public_path( 'vendor/laravel-filemanager' ),
+		], 'lfm_public' );
 
-        $this->publishes([
-            __DIR__.'/views'  => base_path('resources/views/vendor/laravel-filemanager'),
-        ], 'lfm_view');
-    }
+		$this->publishes( [
+			__DIR__ . '/views' => base_path( 'resources/views/vendor/laravel-filemanager' ),
+		], 'lfm_view' );
+	}
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app['laravel-filemanager'] = $this->app->share(function ()
-        {
-            return true;
-        });
-    }
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->singleton( 'laravel-filemanager', function ()
+		{
+			return true;
+		} );
+	}
 
 }
