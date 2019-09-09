c8ace219a62f5cb65e1df807a1332ea9a20a32a9
Jan-Joost <janjoost@u-lab.nl>
2019-09-09 16:36:06 +0200

    [CORE] Remove jQuery requirement on lfm.js

--- public/js/lfm.js
+++ public/js/lfm.js
@@ -1,33 +1,99 @@
-(function( $ ){
+class LFMComponent {
+	constructor(element) {
+		// Set a hash and attach it to the element so we
+		// can resolve our element every time through the hash
+		this.elementHash = Math.random().toString(36).substring(7);
+		element.classList.add('filemanager-' + this.elementHash);
 
-    $.fn.filemanager = function(type = 'image') {
+		this.init();
+	}
 
-        if (type === 'image' || type === 'images') {
-            type = 'Images';
-        } else {
-            type = 'Files';
+	getElement() {
+		return window.document.querySelector('.filemanager-' + this.elementHash);
         }
 
-        let input_id = this.data('input');
-        let preview_id = this.data('preview');
+	/**
+	 * Attach event listeners
+	 */
+	init() {
+		this.getElement().addEventListener('click', () => this.open());
+	}
 
-        this.on('click', function(e) {
-            localStorage.setItem('target_input', input_id);
-            localStorage.setItem('target_preview', preview_id);
-            window.open('/laravel-filemanager?type=' + type, 'FileManager', 'width=900,height=600');
-            return false;
-        });
+	/**
+	 * Detach event listeners
+	 */
+	destroy() {
+		this.getElement().removeEventListener('click', () => this.open());
     }
 
-})(jQuery);
+	/**
+	 * Open the filemanager window
+	 */
+	open() {
+		localStorage.setItem('target_input', this.getElement().dataset.filemanagerInput);
+		localStorage.setItem('target_preview', this.getElement().dataset.filemanagerPreview);
+		window.open('/laravel-filemanager?type=' + this.resolveType(), 'FileManager', 'width=900,height=900');
+	}
 
+	/**
+	 * Resolve type from dataset and resolve
+	 * final type with "Files" as default
+	 *
+	 * @returns {string}
+	 */
+	resolveType() {
+		let dataType = this.getElement().dataset.filemanagerType;
+		if(dataType === 'image' || dataType === 'images') {
+			return 'Images';
+		}
+
+		return 'Files';
+	}
+}
+
+class LFM {
+	constructor() {
+		// Create an array in which we will track all components
+		this.components = [];
+		this.init();
+	}
+
+	/**
+	 * Initialize all components
+	 */
+	init() {
+		let elements = window.document.querySelectorAll('.filemanager');
+		for(let index = 0; index < elements.length; index++) {
+			this.addElement(elements[index]);
+		}
+	}
+
+	/**
+	 * Create a new component from an element
+	 *
+	 * @param element
+	 */
+	addElement(element) {
+		this.components.push(new LFMComponent(element));
+	}
+
+	/**
+	 * Destroy all components
+	 */
+	destroy() {
+		for(let index = 0; index < this.components.length; index++) {
+			this.components[index].destroy();
+			this.components.splice(index);
+		}
+	}
+}
 
 function SetUrl(url){
-  //set the value of the desired input to image url
-  let target_input = $('#' + localStorage.getItem('target_input'));
-  target_input.val(url);
+	// Set the value of the desired input to image url
+	let targetInput = window.document.querySelector('#' + localStorage.getItem('target_input'));
+	targetInput.value = url;
 
-  //set or change the preview image src
-  let target_preview = $('#' + localStorage.getItem('target_preview'));
-  target_preview.attr('src',url);
+	// Set or change the preview image src
+	let targetPreview = window.document.querySelector('#' + localStorage.getItem('target_preview'));
+	targetPreview.setAttribute('src',url);
 }
